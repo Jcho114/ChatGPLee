15de1907bf2df7bc3c03e91782517e2e63e2d838
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2023-11-12 21:12:14 -0500

    added filtering and error handling

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import os
 import re
 import discord
+import datetime
 from os.path import join, dirname
 from dotenv import load_dotenv
+from colorama import Fore
 
 load_dotenv()
 
@@ -14,8 +16,17 @@ client = discord.Client(intents=intents)
 SYS_PROMPT = "<prompt>"
 
 def formatter(Q, R):
-        json_str = f'{{"messages": [{{"role": "system", "content": "{SYS_PROMPT}"}}, {{"role": "user", "content": "{Q}"}}, {{"role": "assistant", "content": "{R}"}}]}}'
-        return json_str
+    R = re.sub("\s<@!\d{18}>\s", "", R)
+    R = re.sub("\s<@\d{18}>\s", "", R)
+    R = re.sub("\s<:.:\d{18}>\s", "", R)
+    json_str = f'{{"messages": [{{"role": "system", "content": "{SYS_PROMPT}"}}, {{"role": "user", "content": "{Q}"}}, {{"role": "assistant", "content": "{R}"}}]}}'
+    return json_str
+
+def isValidMessage(M):
+    M = re.sub("\s<@!\d{18}>\s", "", M)
+    M = re.sub("\s<@\d{18}>\s", "", M)
+    M = re.sub("\s<:.:\d{18}>\s", "", M)
+    return not M.content.startswith("https://") and M.content != "" and not re.match(r'^"', M.content)
 
 @client.event
 async def on_ready():
@@ -29,18 +40,21 @@ async def on_message(message):
         await message.channel.send('Hello!')
     if message.content.startswith('$history') and message.author.name == "jcho_114":
         res = ''
-        if re.match(r"\$history (\w+)", message.content):
-            username = re.match(r"\$history (\w+)", message.content)[1]
+        if re.match(r"\$history (.+)", message.content):
+            username = re.match(r"\$history (.+)", message.content)[1]
         else: await message.channel.send("Invalid Command Format")
-        channels = client.get_all_channels()
-        for channel in channels:
-            if isinstance(channel, discord.TextChannel):
-                async for message in channel.history(limit=200):
-                    if message.author.name.lower() == username.lower():
-                        res += formatter("<Q>", message.content) + '\n'
+        for guild in client.guilds:
+            if guild == message.guild:
+                for channel in guild.channels:
+                    if isinstance(channel, discord.TextChannel):
+                        async for message in channel.history(limit=30000):
+                            if message.author.name.lower() == username.lower() and isValidMessage(message):
+                                print(Fore.GREEN + f'[{datetime.datetime.now()}::MESSAGE FOUND] {message.content}')
+                                res += formatter("<Q>", message.content) + '\n'
         f = open(".txt", "w")
         f.write(res)
         f.close()
+        print(Fore.GREEN + f'[{datetime.datetime.now()}::DONE]')
         await message.channel.send(file=discord.File(".txt"))
 
 client.run(os.environ.get("TOKEN"))
